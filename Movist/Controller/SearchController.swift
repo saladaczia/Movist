6d34b237ecb014778a27c39eeae914572a54bae5
Saladaczia <75745541+saladaczia@users.noreply.github.com>
2022-09-29 12:17:37 +0200

    Create main ui, add all controllers and apply MVC

---
+++ Movist/Controller/SearchController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchController.swift
+//  Movist
+//
+//  Created by Maciej Sołoducha on 29/09/2022.
+//
+
+import UIKit
+
+class SearchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Szukaj filmu"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

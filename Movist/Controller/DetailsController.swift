57919ead162bebaa477e8a18d45478895a62f7bb
Saladaczia <75745541+saladaczia@users.noreply.github.com>
2022-10-05 10:52:48 +0200

    Add function for show all streaming services when you can watch movie

--- Movist/Controller/DetailsController.swift
+++ Movist/Controller/DetailsController.swift
@@ -20,6 +20,24 @@ class DetailsController: UIViewController{
     @IBOutlet weak var StreamigOne: UIImageView!
     @IBOutlet weak var StreamingTwo: UIImageView!
     @IBOutlet weak var StreamingThree: UIImageView!
+    @IBOutlet weak var StreamingFour: UIImageView!
+    @IBOutlet weak var StreamingFive: UIImageView!
+    @IBOutlet weak var StreamingSix: UIImageView!
+    
+    @IBOutlet weak var conOne: NSLayoutConstraint!
+    @IBOutlet weak var conTwo: NSLayoutConstraint!
+    @IBOutlet weak var conThree: NSLayoutConstraint!
+    @IBOutlet weak var conFour: NSLayoutConstraint!
+    @IBOutlet weak var conFive: NSLayoutConstraint!
+    @IBOutlet weak var conSix: NSLayoutConstraint!
+    
+    @IBOutlet weak var spaceTwo: NSLayoutConstraint!
+    @IBOutlet weak var spaceThree: NSLayoutConstraint!
+    @IBOutlet weak var spaceFour: NSLayoutConstraint!
+    @IBOutlet weak var spaceFive: NSLayoutConstraint!
+    @IBOutlet weak var spaceSix: NSLayoutConstraint!
+    
+    
     
     @IBAction func watchTrailer(_ sender: Any) {
         self.performSegue(withIdentifier: "goToWebFromDetails", sender: self)
@@ -34,6 +52,7 @@ class DetailsController: UIViewController{
     
         var movieID = 0
         var movieTitle = ""
+    var providerTable = [String]()
     
     func getDetails() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)?api_key=dfa4cb178f87b623801a1223f21a555d&language=pl-PL")
@@ -49,6 +68,7 @@ class DetailsController: UIViewController{
                     self.yearLabel.text = "\(result.releaseDate.dropLast(6)) | \(result.runtime)min"
                     self.descriptionLabel.text = result.overview
                     self.genereLabel.text = result.genres[0].name
+                    
                 }
             } catch {
                 
@@ -56,7 +76,75 @@ class DetailsController: UIViewController{
         }.resume()
     }
     
-   
+    func getProvider() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieID)/watch/providers?api_key=dfa4cb178f87b623801a1223f21a555d")
+        
+        URLSession.shared.dataTask(with: url!) {
+            (data,req,error) in
+            do {
+                let result = try JSONDecoder().decode(StreamingSchema.self, from: data!)
+                DispatchQueue.main.async {
+                    let myNumber = result.results.pl.flatrate!.count - 1
+                    
+                    for number in 0...myNumber {
+                        self.providerTable.append(result.results.pl.flatrate![number].providerName)
+                        
+                }
+                   
+                    if self.providerTable.contains("Netflix") {
+                        self.StreamigOne.image = UIImage(named: "Netflix")
+                        self.conOne.constant = 48
+                    }
+                
+                    if self.providerTable.contains("Horizon") {
+                        self.StreamingTwo.image = UIImage(named: "Horizon")
+                        self.conOne.constant = 48
+                        if self.providerTable.count > 1 {
+                        self.spaceTwo.constant = 8
+                        }
+                        }
+                    
+                    if self.providerTable.contains("HBO Max") {
+                        self.StreamingThree.image = UIImage(named: "HBO Max")
+                        self.conThree.constant = 48
+                        if self.providerTable.count > 1 {
+                        self.spaceThree.constant = 8
+                        }
+                    }
+                    if self.providerTable.contains("Disney Plus") {
+                        self.StreamingFour.image = UIImage(named: "Disney Plus")
+                        self.conFour.constant = 48
+                        if self.providerTable.count > 1 {
+                        self.spaceFour.constant = 8
+                        }
+                    }
+                    if self.providerTable.contains("Apple TV Plus") {
+                        self.StreamingFive.image = UIImage(named: "Apple TV Plus")
+                        self.conFive.constant = 48
+                        if self.providerTable.count > 1 {
+                        self.spaceFive.constant = 8
+                        }
+                    }
+                    if self.providerTable.contains("Player") {
+                        self.StreamingSix.image = UIImage(named: "Player")
+                        self.conSix.constant = 48
+                        if self.providerTable.count > 1 {
+                        self.spaceSix.constant = 8
+                        }
+                    }
+                    
+
+                    
+                    
+                    
+                    
+                    
+                }
+            } catch {
+                print("error")
+            }
+        }.resume()
+    }
     
   
 
@@ -74,7 +162,7 @@ class DetailsController: UIViewController{
         // Do any additional setup after loading the view.
         
         getDetails()
-        
+        getProvider()
     }
     
 
--- Movist/Model/JSON Schema/StreamingSchema.swift
+++ Movist/Model/JSON Schema/StreamingSchema.swift
@@ -20,198 +20,34 @@ struct StreamingSchema: Codable {
 
 // MARK: - Results
 struct StreamingResults: Codable {
-    let ad: Ad
-    let ae, ar, at, au: AE
-    let ba, bb: Ad
-    let be, bg: AE
-    let bh: Ad
-    let bo, br: AE
-    let bs: Ad
-    let ca, ch: AE
-    let ci: Ad
-    let cl, co, cr, cz: AE
-    let de, dk: AE
-    let resultsDO, dz: Ad
-    let ec, ee, eg, es: AE
-    let fi, fr, gb: AE
-    let gf, gh, gq: Ad
-    let gr, gt, hk, hn: AE
-    let hr, hu, id, ie: AE
-    let il: Ad
-    let resultsIN: AE
-    let iq: Ad
-    let resultsIS, it: AE
-    let jm, jo: Ad
-    let jp: AE
-    let ke: Ad
-    let kr: AE
-    let kw, lb, li: Ad
-    let lt, lv: AE
-    let ly, ma, mc, md: Ad
-    let mk, mt, mu: Ad
-    let mx, my: AE
-    let mz, ne, ng: Ad
-    let nl, no, nz: AE
-    let om, pa: Ad
-    let pe: AE
-    let pf: Ad
-    let ph, pl: AE
-    let ps: Ad
-    let pt, py: AE
-    let qa, ro, rs: Ad
-    let ru, sa: AE
-    let sc: Ad
-    let se, sg, si, sk: AE
-    let sm, sn, sv: Ad
-    let th: AE
-    let tn: Ad
-    let tr: AE
-    let tt: Ad
-    let tw: AE
-    let tz, ug: Ad
-    let us: AE
-    let uy: Ad
-    let ve: AE
-    let ye: Ad
-    let za: AE
-    let zm: Ad
+    
+    let pl: AE
+    
 
     enum CodingKeys: String, CodingKey {
-        case ad = "AD"
-        case ae = "AE"
-        case ar = "AR"
-        case at = "AT"
-        case au = "AU"
-        case ba = "BA"
-        case bb = "BB"
-        case be = "BE"
-        case bg = "BG"
-        case bh = "BH"
-        case bo = "BO"
-        case br = "BR"
-        case bs = "BS"
-        case ca = "CA"
-        case ch = "CH"
-        case ci = "CI"
-        case cl = "CL"
-        case co = "CO"
-        case cr = "CR"
-        case cz = "CZ"
-        case de = "DE"
-        case dk = "DK"
-        case resultsDO = "DO"
-        case dz = "DZ"
-        case ec = "EC"
-        case ee = "EE"
-        case eg = "EG"
-        case es = "ES"
-        case fi = "FI"
-        case fr = "FR"
-        case gb = "GB"
-        case gf = "GF"
-        case gh = "GH"
-        case gq = "GQ"
-        case gr = "GR"
-        case gt = "GT"
-        case hk = "HK"
-        case hn = "HN"
-        case hr = "HR"
-        case hu = "HU"
-        case id = "ID"
-        case ie = "IE"
-        case il = "IL"
-        case resultsIN = "IN"
-        case iq = "IQ"
-        case resultsIS = "IS"
-        case it = "IT"
-        case jm = "JM"
-        case jo = "JO"
-        case jp = "JP"
-        case ke = "KE"
-        case kr = "KR"
-        case kw = "KW"
-        case lb = "LB"
-        case li = "LI"
-        case lt = "LT"
-        case lv = "LV"
-        case ly = "LY"
-        case ma = "MA"
-        case mc = "MC"
-        case md = "MD"
-        case mk = "MK"
-        case mt = "MT"
-        case mu = "MU"
-        case mx = "MX"
-        case my = "MY"
-        case mz = "MZ"
-        case ne = "NE"
-        case ng = "NG"
-        case nl = "NL"
-        case no = "NO"
-        case nz = "NZ"
-        case om = "OM"
-        case pa = "PA"
-        case pe = "PE"
-        case pf = "PF"
-        case ph = "PH"
+       
         case pl = "PL"
-        case ps = "PS"
-        case pt = "PT"
-        case py = "PY"
-        case qa = "QA"
-        case ro = "RO"
-        case rs = "RS"
-        case ru = "RU"
-        case sa = "SA"
-        case sc = "SC"
-        case se = "SE"
-        case sg = "SG"
-        case si = "SI"
-        case sk = "SK"
-        case sm = "SM"
-        case sn = "SN"
-        case sv = "SV"
-        case th = "TH"
-        case tn = "TN"
-        case tr = "TR"
-        case tt = "TT"
-        case tw = "TW"
-        case tz = "TZ"
-        case ug = "UG"
-        case us = "US"
-        case uy = "UY"
-        case ve = "VE"
-        case ye = "YE"
-        case za = "ZA"
-        case zm = "ZM"
+       
     }
 }
 
-// MARK: - Ad
-struct Ad: Codable {
-    let link: String
-    let flatrate: [Flatrate]
-}
 
 // MARK: - Flatrate
-struct Flatrate: Codable {
-    let logoPath: String
-    let providerID: Int
+struct StreamingFlatrate: Codable {
+    
+   
     let providerName: String
-    let displayPriority: Int
+    
 
     enum CodingKeys: String, CodingKey {
-        case logoPath = "logo_path"
-        case providerID = "provider_id"
+        
         case providerName = "provider_name"
-        case displayPriority = "display_priority"
+        
     }
 }
 
 // MARK: - AE
 struct AE: Codable {
-    let link: String
-    let buy: [Flatrate]
-    let flatrate, rent, free: [Flatrate]?
+    
+    let flatrate: [StreamingFlatrate]?
 }
-
